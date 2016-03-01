2d2b590854b3be3c7fc44521165643ef90b82b97
JayT5ai <jtsai778@gmail.com>
2016-02-29 18:48:58 -0800

    Added Delete, Filter, Ext, Add

--- Compressionator_JayTsai_113719/Compressionator_JayTsai_113719/Form1.Designer.cs
+++ Compressionator_JayTsai_113719/Compressionator_JayTsai_113719/Form1.Designer.cs
@@ -45,6 +45,15 @@
             this.PathText3 = new System.Windows.Forms.TextBox();
             this.folderPath3 = new System.Windows.Forms.Button();
             this.ExtractButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.FilterBox = new System.Windows.Forms.ComboBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SourceBox = new System.Windows.Forms.TextBox();
+            this.folderPath5 = new System.Windows.Forms.Button();
+            this.RenameBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.ActionCheck = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // folderPath1
@@ -66,7 +75,7 @@
             // 
             // CompressButton
             // 
-            this.CompressButton.Location = new System.Drawing.Point(340, 132);
+            this.CompressButton.Location = new System.Drawing.Point(241, 131);
             this.CompressButton.Name = "CompressButton";
             this.CompressButton.Size = new System.Drawing.Size(75, 23);
             this.CompressButton.TabIndex = 5;
@@ -78,9 +87,10 @@
             // 
             this.ExtBox.FormattingEnabled = true;
             this.ExtBox.Items.AddRange(new object[] {
-            "zip",
-            "gzip"});
-            this.ExtBox.Location = new System.Drawing.Point(213, 134);
+            "",
+            ".zip",
+            ".gz"});
+            this.ExtBox.Location = new System.Drawing.Point(114, 133);
             this.ExtBox.Name = "ExtBox";
             this.ExtBox.Size = new System.Drawing.Size(121, 21);
             this.ExtBox.TabIndex = 6;
@@ -123,7 +133,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(210, 118);
+            this.label3.Location = new System.Drawing.Point(111, 117);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 11;
@@ -132,7 +142,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(114, 215);
+            this.label4.Location = new System.Drawing.Point(114, 305);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(59, 13);
             this.label4.TabIndex = 14;
@@ -140,14 +150,14 @@
             // 
             // PathText4
             // 
-            this.PathText4.Location = new System.Drawing.Point(114, 234);
+            this.PathText4.Location = new System.Drawing.Point(114, 324);
             this.PathText4.Name = "PathText4";
             this.PathText4.Size = new System.Drawing.Size(332, 20);
             this.PathText4.TabIndex = 13;
             // 
             // folderPath4
             // 
-            this.folderPath4.Location = new System.Drawing.Point(452, 232);
+            this.folderPath4.Location = new System.Drawing.Point(452, 322);
             this.folderPath4.Name = "folderPath4";
             this.folderPath4.Size = new System.Drawing.Size(75, 23);
             this.folderPath4.TabIndex = 12;
@@ -158,7 +168,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(114, 176);
+            this.label5.Location = new System.Drawing.Point(114, 266);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(71, 13);
             this.label5.TabIndex = 17;
@@ -166,14 +176,14 @@
             // 
             // PathText3
             // 
-            this.PathText3.Location = new System.Drawing.Point(114, 192);
+            this.PathText3.Location = new System.Drawing.Point(114, 282);
             this.PathText3.Name = "PathText3";
             this.PathText3.Size = new System.Drawing.Size(332, 20);
             this.PathText3.TabIndex = 16;
             // 
             // folderPath3
             // 
-            this.folderPath3.Location = new System.Drawing.Point(452, 190);
+            this.folderPath3.Location = new System.Drawing.Point(452, 280);
             this.folderPath3.Name = "folderPath3";
             this.folderPath3.Size = new System.Drawing.Size(75, 23);
             this.folderPath3.TabIndex = 15;
@@ -183,7 +193,7 @@
             // 
             // ExtractButton
             // 
-            this.ExtractButton.Location = new System.Drawing.Point(259, 260);
+            this.ExtractButton.Location = new System.Drawing.Point(259, 350);
             this.ExtractButton.Name = "ExtractButton";
             this.ExtractButton.Size = new System.Drawing.Size(75, 23);
             this.ExtractButton.TabIndex = 18;
@@ -191,11 +201,104 @@
             this.ExtractButton.UseVisualStyleBackColor = true;
             this.ExtractButton.Click += new System.EventHandler(this.ExtractButton_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(331, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Filter";
+            // 
+            // FilterBox
+            // 
+            this.FilterBox.FormattingEnabled = true;
+            this.FilterBox.Items.AddRange(new object[] {
+            "",
+            ".jpg",
+            ".txt"});
+            this.FilterBox.Location = new System.Drawing.Point(334, 133);
+            this.FilterBox.Name = "FilterBox";
+            this.FilterBox.Size = new System.Drawing.Size(121, 21);
+            this.FilterBox.TabIndex = 20;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(452, 223);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 22;
+            this.AddButton.Text = "Add To Zip";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // SourceBox
+            // 
+            this.SourceBox.Location = new System.Drawing.Point(114, 193);
+            this.SourceBox.Name = "SourceBox";
+            this.SourceBox.Size = new System.Drawing.Size(332, 20);
+            this.SourceBox.TabIndex = 24;
+            // 
+            // folderPath5
+            // 
+            this.folderPath5.Location = new System.Drawing.Point(452, 191);
+            this.folderPath5.Name = "folderPath5";
+            this.folderPath5.Size = new System.Drawing.Size(75, 23);
+            this.folderPath5.TabIndex = 23;
+            this.folderPath5.Text = "Browse";
+            this.folderPath5.UseVisualStyleBackColor = true;
+            this.folderPath5.Click += new System.EventHandler(this.folderPath5_Click);
+            // 
+            // RenameBox
+            // 
+            this.RenameBox.Location = new System.Drawing.Point(114, 223);
+            this.RenameBox.Name = "RenameBox";
+            this.RenameBox.Size = new System.Drawing.Size(332, 20);
+            this.RenameBox.TabIndex = 25;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(111, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Source File To Rename";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(259, 160);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(124, 23);
+            this.DeleteButton.TabIndex = 27;
+            this.DeleteButton.Text = "Filter Out Ext From Zip";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // ActionCheck
+            // 
+            this.ActionCheck.AutoSize = true;
+            this.ActionCheck.Location = new System.Drawing.Point(452, 398);
+            this.ActionCheck.Name = "ActionCheck";
+            this.ActionCheck.Size = new System.Drawing.Size(77, 17);
+            this.ActionCheck.TabIndex = 28;
+            this.ActionCheck.Text = "Log Action";
+            this.ActionCheck.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(687, 589);
+            this.Controls.Add(this.ActionCheck);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.RenameBox);
+            this.Controls.Add(this.SourceBox);
+            this.Controls.Add(this.folderPath5);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.FilterBox);
             this.Controls.Add(this.ExtractButton);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.PathText3);
@@ -238,6 +341,15 @@
         private System.Windows.Forms.TextBox PathText3;
         private System.Windows.Forms.Button folderPath3;
         private System.Windows.Forms.Button ExtractButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox FilterBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox SourceBox;
+        private System.Windows.Forms.Button folderPath5;
+        private System.Windows.Forms.TextBox RenameBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.CheckBox ActionCheck;
     }
 }
 
--- Compressionator_JayTsai_113719/Compressionator_JayTsai_113719/Form1.cs
+++ Compressionator_JayTsai_113719/Compressionator_JayTsai_113719/Form1.cs
@@ -12,8 +12,11 @@ using System.IO.Compression;
 
 namespace Compressionator_JayTsai_113719
 {
+    
     public partial class Form1 : Form
     {
+        string pathZip;
+        OpenFileDialog fDialog = new OpenFileDialog();
         public Form1()
         {
             InitializeComponent();
@@ -27,17 +30,185 @@ namespace Compressionator_JayTsai_113719
             {
                 this.PathText1.Text = folderBrowserDialog1.SelectedPath;
             }
+            
+            
         }
 
         private void CompressButton_Click(object sender, EventArgs e)
         {
+            if(ExtBox.Text == "")
+            {
+                ExtBox.Text = ".zip";
+            }
+            
+            DirectoryInfo directorySelected;
             if(ExtBox.Text != "")
             {
-                if (PathText2.Text == "")
+                if(ExtBox.Text == ".zip")
                 {
-                    PathText2.Text = PathText1.Text + "." + ExtBox.Text;
+                    if (FilterBox.Text != "")
+                    {
+                        if (PathText2.Text == "")
+                        {
+                            directorySelected = new DirectoryInfo(PathText1.Text);
+                            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+                            {
+                                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                                {
+                                    if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden && fileToCompress.Extension != FilterBox.Text)
+                                    {
+                                        using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ExtBox.Text))
+                                        {
+                                            using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                                            {
+                                                originalFileStream.CopyTo(compressionStream);
+                                            }
+                                        }
+                                        FileInfo info = new FileInfo(directorySelected.FullName + "\\" + fileToCompress.Name + ExtBox.Text);
+                                        
+                                    }
+                                }
+                            }
+                            PathText2.Text = directorySelected.FullName;
+                        }
+                        else if (PathText2.Text != "")
+                        {
+                            directorySelected = new DirectoryInfo(PathText1.Text);
+                            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+                            {
+                                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                                {
+                                    if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden && fileToCompress.Extension != FilterBox.Text)
+                                    {
+                                        using (FileStream compressedFileStream = File.Create(PathText2.Text + "\\" + fileToCompress.Name + ExtBox.Text))
+                                        {
+                                            using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                                            {
+                                                originalFileStream.CopyTo(compressionStream);
+                                            }
+                                        }
+                                        FileInfo info = new FileInfo(PathText2.Text + "\\" + fileToCompress.Name + ExtBox.Text);
+                                        
+                                    }
+                                }
+                            }
+                        }
+                        else if (File.Exists(pathZip))
+                        {
+                            System.Windows.Forms.MessageBox.Show("File Already Existed");
+                        }
+                    }
+                    else if (PathText2.Text == "")
+                    {
+                        pathZip = PathText1.Text + ExtBox.Text;
+                        PathText2.Text = PathText1.Text;
+                        if (!File.Exists(pathZip))
+                        {
+                            ZipFile.CreateFromDirectory(PathText1.Text, pathZip);
+                        }
+                        else
+                        {
+                            System.Windows.Forms.MessageBox.Show("File Already Existed");
+                        }
+                    }
+                    else
+                    {
+                        pathZip = PathText2.Text + "\\" + Path.GetFileName(PathText1.Text) + ExtBox.Text;
+                        
+                        if (!File.Exists(pathZip))
+                        {
+                            ZipFile.CreateFromDirectory(PathText1.Text, pathZip);
+                        }
+                        else
+                        {
+                            System.Windows.Forms.MessageBox.Show("File Already Existed");
+                        }
+                    }
                 }
-                ZipFile.CreateFromDirectory(PathText1.Text, PathText2.Text);
+                else if(ExtBox.Text == ".gz")
+                {
+                    
+                    if(FilterBox.Text != "")
+                    {
+                        if (PathText2.Text == "")
+                        {
+                            directorySelected = new DirectoryInfo(PathText1.Text);
+                            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+                            {
+                                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                                {
+                                    if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden && fileToCompress.Extension != FilterBox.Text)
+                                    {
+                                        using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ExtBox.Text))
+                                        {
+                                            using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                                            {
+                                                originalFileStream.CopyTo(compressionStream);
+                                            }
+                                        }
+                                        FileInfo info = new FileInfo(directorySelected.FullName + "\\" + fileToCompress.Name + ExtBox.Text);
+                                        
+                                    }
+                                }
+                            }
+                            PathText2.Text = directorySelected.FullName;
+                        }
+                        else if (PathText2.Text != "")
+                        {
+                            directorySelected = new DirectoryInfo(PathText1.Text);
+                            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+                            {
+                                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                                {
+                                    if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden && fileToCompress.Extension != FilterBox.Text)
+                                    {
+                                        using (FileStream compressedFileStream = File.Create(PathText2.Text + "\\" + Path.GetFileName(PathText1.Text) + ExtBox.Text))
+                                        {
+                                            using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                                            {
+                                                originalFileStream.CopyTo(compressionStream);
+                                            }
+                                        }
+                                        FileInfo info = new FileInfo(PathText2.Text + "\\" + Path.GetFileName(PathText1.Text) + ExtBox.Text);
+                                        
+                                    }
+                                }
+                            }
+                        }
+                        else if (File.Exists(pathZip))
+                        {
+                            System.Windows.Forms.MessageBox.Show("File Already Existed");
+                        }
+                    }
+                    else if (PathText2.Text == "")
+                    {
+                        pathZip = PathText1.Text + ExtBox.Text;
+                        PathText2.Text = PathText1.Text;
+                        if (!File.Exists(pathZip))
+                        {
+                            ZipFile.CreateFromDirectory(PathText1.Text, pathZip);
+                        }
+                        else
+                        {
+                            System.Windows.Forms.MessageBox.Show("File Already Existed");
+                        }
+                    }
+                    else
+                    {
+                        pathZip = PathText2.Text + "\\" + Path.GetFileName(PathText1.Text) + ExtBox.Text;
+                        if (!File.Exists(pathZip))
+                        {
+                            ZipFile.CreateFromDirectory(PathText1.Text, pathZip);
+                        }
+                        else
+                        {
+                            System.Windows.Forms.MessageBox.Show("File Already Existed");
+                        }
+                    }
+                    
+
+                    
+                }
             }
             else
             {
@@ -54,9 +225,10 @@ namespace Compressionator_JayTsai_113719
         }
         private void folderPath3_Click(object sender, EventArgs e)
         {
-            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            DialogResult result = fDialog.ShowDialog();
+            if( result == DialogResult.OK ) // Test result.
             {
-                this.PathText3.Text = folderBrowserDialog1.SelectedPath;
+                this.PathText3.Text = fDialog.FileName;
             }
         }
 
@@ -72,7 +244,7 @@ namespace Compressionator_JayTsai_113719
         {
             if (PathText3.Text == "")
             {
-                PathText3.Text = PathText2.Text;
+                PathText3.Text = pathZip;
             }
             if (PathText4.Text == "")
             {
@@ -85,6 +257,45 @@ namespace Compressionator_JayTsai_113719
                 System.Windows.Forms.MessageBox.Show("Please input what you want to extract.");
             }
             
+        }
+
+
+
+        private void folderPath5_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fDialog.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                SourceBox.Text = fDialog.FileName;
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            ZipArchiveMode mode = ZipArchiveMode.Create;
+
+            using (ZipArchive zipFile = ZipFile.Open(pathZip, mode))
+            {
+                
+            }
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            using (ZipArchive modFile = ZipFile.Open(pathZip, ZipArchiveMode.Update))
+            {
+                if (SourceBox.Text != "" && RenameBox.Text != "")
+                {
+                    modFile.CreateEntryFromFile(SourceBox.Text, RenameBox.Text);
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Please input a new name or Source file for rename.");
+                }
+
+
+            }
         }
 
        
